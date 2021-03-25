32817a445cadb5e1d8c2f36f3b5eae9b877af20c
Tehnorobot <75486437+Tehnorobot@users.noreply.github.com>
2021-03-25 19:25:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
+    db_sess = db_session.create_session()
+    db_sess.add(job)
+    db_sess.commit()
+
+
+if __name__ == '__main__':
+    main()
